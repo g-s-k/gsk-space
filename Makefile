f2528434663567d9305cade02a9d237a25a7b129
George Kaplan <george@georgekaplan.xyz>
2018-11-25 19:12:59 -0500

    added (useless) backend service

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ all: build
 
 build:
 	cd client; yarn build
+	cd server; cargo build --release --target=x86_64-unknown-linux-musl
 
 deploy: build
 	docker-compose up -d --build
---
+++ server/src/main.rs
@@ -0,0 +1,15 @@
+extern crate actix_web;
+
+use actix_web::{server, App, HttpRequest};
+
+fn index(req: &HttpRequest) -> &'static str {
+    println!("{:?}", req);
+    "Hello World!"
+}
+
+fn main() {
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind("0.0.0.0:8000")
+        .unwrap()
+        .run();
+}
