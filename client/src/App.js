5d7bac1aeb8887ee498e0980d5b2dd072fb6d0dc
George Kaplan <george@georgekaplan.xyz>
2018-11-26 22:31:44 -0500

    added call to new endpoint from frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,53 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      message: ""
+      message: "",
+      random: ""
     };
   }
 
   async componentWillMount() {
     const message = await fetch("/api").then(x => x.json());
-    this.setState({ message });
+    const random = await fetch("/api/sha256").then(x => x.text());
+    this.setState({ message, random });
   }
 
   render() {
-    const { message } = this.state;
+    const { message, random } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to George's website</h1>
         </header>
-        <p className="App-intro">
-          {message ? `A message from the API: ${message}` : "No message from the API yet."}
-        </p>
-      <footer>
-        <p className="App-info">The source for this site is located <a href="https://github.com/g-s-k/gsk-space">here</a>.</p>
+      <div className="App-body">
+        <div>
+          <div>
+            {message
+              ? `A message from the API: ${message}`
+              : "No message from the API yet."}
+          </div>
+          <div>
+            {random ? (
+              <span>
+                A timestamped hash of an API request: <code>{random}</code>
+              </span>
+            ) : (
+              "No random response from the API yet."
+            )}
+          </div>
+        </div>
+        <footer>
+          <p className="App-info">
+            The source for this site is located{" "}
+            <a href="https://github.com/g-s-k/gsk-space">here</a>.
+          </p>
       </footer>
+      </div>
       </div>
     );
   }
